9be8fdd5ddced60765b3639d88cfd0c4c42000db
Assassin Nima <88039431+n-ii-ma@users.noreply.github.com>
2021-12-13 21:29:09 +0330

    Gallery & Video

--- src/features/post/RedditPost.js
+++ src/features/post/RedditPost.js
@@ -1,13 +1,52 @@
 const RedditPost = ({ post }) => {
+  /*   const galleryImages = () => {
+    if (post.is_gallery) {
+      post.gallery_data.items.map((item) => (
+        <img
+          src={`https://i.redd.it/${item.media_id}.jpg`}
+          className="image"
+          alt="Gallery Images"
+        />
+      ));
+    }
+  }; */
+
+  // Check if post is a gallery and map through each image and get the id
+  /*   if (post.is_gallery) {
+    const test = post.gallery_data.items.map((item) => item.media_id);
+    console.log(test);
+  } */
+
   return (
     <div className="box">
       <p className="upvotes">{post.ups} Upvotes</p>
       <p className="author">u/{post.author}</p>
       <p className="title">{post.title}</p>
-      {post.url.endsWith(".jpg") || post.url.endsWith(".gif") ? (
-        <img src={post.url} alt="Subreddit Images" />
+      {post.url.endsWith(".jpg") ||
+      post.url.endsWith(".gif") ||
+      post.url.endsWith(".png") ? (
+        <img src={post.url} className="image" alt="Subreddit Images" />
       ) : (
-        <p className="error">Image Not Available</p>
+        ""
+      )}
+      {post.is_gallery
+        ? post.gallery_data.items.map((item) => (
+            <div key={item.media_id} className="gallery">
+              <img
+                src={`https://i.redd.it/${item.media_id}.jpg`}
+                className="image"
+                alt="Gallery Images"
+              />
+            </div>
+          ))
+        : ""}
+      {post.is_video ? (
+        <video controls className="video">
+          <source src={post.media.reddit_video.fallback_url} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      ) : (
+        ""
       )}
     </div>
   );
