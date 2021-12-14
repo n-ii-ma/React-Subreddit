d6619fa6580eb71f7d94c45ca173b5c8e70e1c01
Assassin Nima <88039431+n-ii-ma@users.noreply.github.com>
2021-12-14 22:35:19 +0330

    Error Message Update

--- src/components/HasError.js
+++ src/components/HasError.js
@@ -2,7 +2,7 @@ const HasError = () => {
   return (
     <div>
       <p className="error">Couldn't Reach Reddit Servers!<br />Please Try Again</p>
-      <p className="hint">Hint:<br />Make Sure to Write the Subreddit Correctly</p>
+      <p className="hint">Hint:<br />Make Sure to Write the Subreddit Name Correctly</p>
     </div>
   );
 };
